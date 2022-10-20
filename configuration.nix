6cf2733bc88a17da138a7fe7dbe0b3c3aeba69b0
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2022-10-20 20:49:25 +0700

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,210 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "msi"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "id_ID.utf8";
+    LC_IDENTIFICATION = "id_ID.utf8";
+    LC_MEASUREMENT = "id_ID.utf8";
+    LC_MONETARY = "id_ID.utf8";
+    LC_NAME = "id_ID.utf8";
+    LC_NUMERIC = "id_ID.utf8";
+    LC_PAPER = "id_ID.utf8";
+    LC_TELEPHONE = "id_ID.utf8";
+    LC_TIME = "id_ID.utf8";
+  };
+
+  # Nix Configuration
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    # Garbage collection
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 4w";
+    };
+  };
+
+  # Enable docker
+  virtualisation.docker.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.halcyon = {
+    isNormalUser = true;
+    description = "halcyon";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      firefox
+      kate
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "halcyon";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable KDE Connect
+  programs.kdeconnect.enable = true;
+
+  # Enable HUION tablet support
+  services.xserver.digimend.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = [
+  # College
+  pkgs.octaveFull
+
+  # Productivity
+  pkgs.slack
+  pkgs.tdesktop # telegram
+  pkgs.zoom-us
+  pkgs.libreoffice
+  pkgs.xournalpp
+
+  # Development
+  pkgs.niv # nix helper to adding and updating dependencies
+  
+  ## Editor
+  pkgs.neovim
+  pkgs.sublime4
+  pkgs.vscode-fhs
+  pkgs.jetbrains.idea-ultimate
+  
+  ## Tools
+  pkgs.git
+  pkgs.docker
+  pkgs.docker-compose_2
+  
+  ## Haskell
+  pkgs.ghc
+  #pkgs.haskellPackages.ghcup
+
+  ## Java
+  pkgs.jdk8
+  pkgs.gradle
+
+  # Testing
+  pkgs.postman
+
+  # OS Utilities
+  pkgs.bind # installs dnsutils dev host lib man out
+  pkgs.wget
+  pkgs.kitty
+  pkgs.zsh
+  pkgs.fzf
+  pkgs.zip
+
+  # Plugins
+  pkgs.oh-my-zsh
+  pkgs.fzf-zsh
+  pkgs.zsh-fzf-tab
+  ];
+
+  # Java
+  # Setup JAVA_HOME system wide
+  programs.java.enable = true;
+  programs.java.package = pkgs.jdk8;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
+
+
